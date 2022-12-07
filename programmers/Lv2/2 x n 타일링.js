7e8e324d8f1d58c7ff9817f31f445a17ed1809c8
Seoyeong Lee <66051416+leeseooo@users.noreply.github.com>
2022-12-07 16:17:55 +0900

    Create 2 x n 타일링.js

---
+++ programmers/Lv2/2 x n 타일링.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    let dp = Array(60000).fill(0);
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    for(let i=3;i<=n;i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+    }
+    answer = dp[n];
+    return answer;
+}
